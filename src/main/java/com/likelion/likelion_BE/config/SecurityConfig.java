bcd9813f9ae23629289bfd64373bc2afe68bfad0
dyongini <abkimdoyo25@gmail.com>
2026-09-01 15:04:06 +0300

    [Feat] CORS 설정

--- src/main/java/com/likelion/likelion_BE/config/SecurityConfig.java
+++ src/main/java/com/likelion/likelion_BE/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.likelion.likelion_BE.config;
 
-
 import com.likelion.likelion_BE.config.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +13,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 적용
                 .csrf(csrf -> csrf.disable())
                 .formLogin(form -> form.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
@@ -63,7 +69,6 @@ public class SecurityConfig {
                                 "/api/v1/sessions/comments/**",
                                 "/api/v1/applications/**",
                                 "/api/v1/stamps/**"
-
                         ).hasAnyAuthority("MEMBER", "LEADER", "MANAGER")
 
                         // 관리자 전용 API
@@ -77,5 +82,28 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 허용할 프론트엔드 등록
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://sswulikelion.com"
+        ));
+
+        // 허용할 HTTP 메서드
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        // 허용할 헤더
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
